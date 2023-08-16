82e0791ecc36b6363435266e090a0851c7f903bc
pulo0 <bartexx13042005@gmail.com>
2023-08-16 19:22:26 +0200

    Added kata Double Char

---
+++ Double Char/dchar.js
@@ -0,0 +1,19 @@
+/* 
+Description:
+
+Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+Examples (Input -> Output):
+
+* "String"      -> "SSttrriinngg"
+* "Hello World" -> "HHeelllloo  WWoorrlldd"
+* "1234!_ "     -> "11223344!!__  "
+
+Good Luck!
+
+Tags: FUNDAMENTALS, STRINGS
+
+*/
+
+function doubleChar(str) {
+    return str.replace(/./g, '$&$&');
+  }
