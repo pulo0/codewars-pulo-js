db14a126410b99be8431815a3eca6807a6f5d704
pulo0 <bartexx13042005@gmail.com>
2023-08-17 22:54:50 +0200

    Added kata Nth term...

---
+++ Sum of the first nth term of Series/nthTerm.js
@@ -0,0 +1,39 @@
+/*
+    Description:
+    Task:
+
+    Your task is to write a function which returns the sum of following series upto nth term(parameter).
+
+    Series: 1 + 1/4 + 1/7 + 1/10 + 1/13 + 1/16 +...
+
+    Rules:
+
+        You need to round the answer to 2 decimal places and return it as String.
+
+        If the given value is 0 then it should return 0.00
+
+        You will only be given Natural Numbers as arguments.
+
+    Examples:(Input --> Output)
+
+    1 --> 1 --> "1.00"
+    2 --> 1 + 1/4 --> "1.25"
+    5 --> 1 + 1/4 + 1/7 + 1/10 + 1/13 --> "1.57"
+
+    Tags: FUNDAMENTALS
+*/
+
+function SeriesSum(n) {
+    let termToChange = 4;
+    let sum = 1;
+
+    if (n == 0 || n == 1) {
+      return String(n.toFixed(2));
+    } else {      
+        for (let i = 1; i < n; i++) {
+          sum += (1 / termToChange);
+          termToChange += 3;
+        }
+      return String(sum.toFixed(2)); 
+    }
+  }
