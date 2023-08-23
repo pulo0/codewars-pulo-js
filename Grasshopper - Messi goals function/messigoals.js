10c47fed1bcbd3c7646e6ad36932733d50bcee85
pulo0 <bartexx13042005@gmail.com>
2023-08-23 21:40:19 +0200

    Added 2 katas First kata is named - 'Grasshopper - Messi goals function' And the second one is named - 'Student's Final Grade'

---
+++ Grasshopper - Messi goals function/messigoals.js
@@ -0,0 +1,29 @@
+/* 
+    DESCRIPTION:
+    Messi goals function
+    Messi is a soccer player with goals in three leagues:
+
+    LaLiga
+    Copa del Rey
+    Champions
+    Complete the function to return his total number of goals in all three leagues.
+
+    Note: the input will always be valid.
+
+    For example:
+
+    5, 10, 2  -->  17
+
+    Tags: FUNDAMENTALS
+
+*/
+
+//First Solution
+
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+    return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+}
+
+//Second solution
+
+const goals = (laLigaGoals, copaDelReyGoals, championsLeagueGoals) => laLigaGoals + copaDelReyGoals + championsLeagueGoals
