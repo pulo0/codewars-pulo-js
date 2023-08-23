d2211090150225b21dbd03069dde558360892ae5
pulo0 <bartexx13042005@gmail.com>
2023-08-23 11:47:01 +0200

    Added a Powers of 2 kata

---
+++ Powers of 2/powersoftwo.js
@@ -0,0 +1,22 @@
+/*
+
+    DESCRIPTION:
+    Complete the function that takes a non-negative integer n as input, and returns a list of all the powers of 2 with the exponent ranging from 0 to n ( inclusive ).
+
+    Examples
+    n = 0  ==> [1]        # [2^0]
+    n = 1  ==> [1, 2]     # [2^0, 2^1]
+    n = 2  ==> [1, 2, 4]  # [2^0, 2^1, 2^2]
+
+    Tags: MATHEMATICS, FUNDAMENTALS
+*/
+
+function powersOfTwo(n){
+    const arr = [];
+    const powerToTwo = 2;
+      for(let i = 0; i <= n; i++) {
+      arr.push(powerToTwo**i);
+      }
+      return arr;
+  }
+
