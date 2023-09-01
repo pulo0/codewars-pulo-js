45a12b478cbc7819c36ea487b23956a2e250d765
pulo0 <bartexx13042005@gmail.com>
2023-09-01 16:41:30 +0200

    Added kata 'Reversed sequence'

---
+++ Reversed sequence/revseq.js
@@ -0,0 +1,16 @@
+/*
+    DESCRIPTION:
+    Build a function that returns an array of integers from n to 1 where n>0.
+
+    Example : n=5 --> [5,4,3,2,1]
+
+    Tags: FUNDAMENTALS
+*/
+
+const reverseSeq = n => {
+    let arr = [];
+    for(let i = 1; i < n + 1; i++) {
+      arr.push(i);
+    };
+    return arr.reverse();
+};
