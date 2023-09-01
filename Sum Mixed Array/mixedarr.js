ec09a556394e07651886e53826a755518822da91
pulo0 <bartexx13042005@gmail.com>
2023-09-01 11:58:53 +0200

    Added kata 'Sum Mixed Array'

---
+++ Sum Mixed Array/mixedarr.js
@@ -0,0 +1,18 @@
+/*
+    DESCRIPTION:
+
+    Given an array of integers as strings and numbers, 
+    return the sum of the array values as if all were numbers.
+
+    Return your answer as a number.
+
+    Tags: STRINGS, ARRAYS, FUNDAMENTALS
+*/
+
+function sumMix(x){
+    let sum = 0;
+    for (let i = 0; i < x.length; i++) {
+      sum += Number(x[i]);
+    }
+    return sum;
+}
