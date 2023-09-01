c3fa4d2c9f5c1de1a6ed8b349cf576e4083d99d2
pulo0 <bartexx13042005@gmail.com>
2023-09-02 00:48:19 +0200

    Added two katas: 'Function 2 - squaring an argument' 'Function 3 - multiplying two numbers (retired kata)'

---
+++ Function 2 - squaring an argument/sqrtarg.js
@@ -0,0 +1,8 @@
+/*
+    DESCRIPTION:
+    Now you have to write a function that takes an argument and returns the square of it.
+
+    Tags: FUNDAMENTALS
+*/
+
+const square = (a) => Math.pow(a,2);
---
+++ Function 3 - multiplying two numbers (retired)/multiply.js
@@ -0,0 +1,12 @@
+/*
+    DESCRIPTION:
+    Implement a function called multiply, which takes two numbers and returns their product:
+
+    multiply(2, 3) = 6
+    multiply(0, 188) = 0
+    multiply(85, 144) = 12240
+
+    Tags: FUNDAMENTALS
+*/
+
+const multiply = (a, b) => a*b;
