d8f81004a3364fc10fef7bdcaf796cfc71dffed0
pulo0 <bartexx13042005@gmail.com>
2023-09-05 20:41:37 +0200

    Added kata 'Convert Boolean to a String'

---
+++ Convert a Boolean to a String/booltostr.js
@@ -0,0 +1,20 @@
+/*
+    DESCRIPTION:
+    Implement a function which convert the given boolean value into its string representation.
+
+    Note: Only valid inputs will be given.
+
+    Tags: STRINGS, FUNDAMENTALS
+*/
+
+//First Solution
+
+function booleanToString(b){
+    return String(b);
+}
+
+//Second Solution
+
+function booleanToString(b){
+    return b.toString();
+  }
