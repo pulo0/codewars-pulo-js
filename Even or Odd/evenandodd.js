a6d099aa9f009b850b46cd0f7e2e7b4716a29c3b
pulo0 <bartexx13042005@gmail.com>
2023-09-05 20:38:45 +0200

    Added kata 'Even or Odd'

---
+++ Even or Odd/evenandodd.js
@@ -0,0 +1,10 @@
+/*
+    DESCRIPTION:
+    Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+    Tags: MATHEMATICS, FUNDAMENTALS
+*/
+
+function evenOrOdd(number) {
+    return number%2==0 ? 'Even' : 'Odd'
+}
