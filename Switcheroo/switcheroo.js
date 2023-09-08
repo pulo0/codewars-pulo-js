b640760b5128bbc4c1febf6cd7dca2090cf8e6d9
pulo0 <bartexx13042005@gmail.com>
2023-09-08 22:56:05 +0200

    Added kata 'Switcheroo'

---
+++ Switcheroo/switcheroo.js
@@ -0,0 +1,21 @@
+/*
+    DESCRIPTION:
+    Given a string made up of letters a, b, and/or c, switch the position 
+    of letters a and b (change a to b and vice versa). Leave any incidence of c untouched.
+
+    Example:
+
+    'acb' --> 'bca'
+    'aabacbaa' --> 'bbabcabb'
+
+    Tags: STRINGS, FUNDAMENTALS
+*/
+
+function switcheroo(x){
+    x = x.split('');
+    for(let i = 0; i < x.length; i++) {
+      if (x[i] == 'a') x[i] = 'b';
+      else if (x[i] == 'b') x[i] = 'a';
+    }
+    return x.join('');
+}
